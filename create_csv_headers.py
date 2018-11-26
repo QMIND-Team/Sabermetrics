3368b4d099836305143c42fd40b31b9f702c1cff
Michael Kerr <mikekerr247@hotmail.com>
2018-11-26 16:18:28 -0500

    added fangraphs csv function

--- create_csv_headers.py
+++ create_csv_headers.py
@@ -9,5 +9,17 @@ def team_pitching_csv():
     writer.writerow(headers)
     out_file.close()
 
+def fan_graphs_csv():
+    # get a list of the headers for the function
+    pitching_statistics = pitching_stats(2016, 2017)
+    pitch_stats_df = pd.DataFrame(pitching_statistics)
+    headers = np.asarray(pitch_stats_df.columns)
+
+    with open("fanGraphs.csv", 'w') as csvFile:
+        wr = csv.writer(csvFile, dialect='excel')
+        wr.writerow(headers)
+    csvFile.close()
+
 
 team_pitching_csv()
+fan_graphs_csv()
