5a4ba6601724c50b36667dce6566de06d9990105
Mike <mikekerr247@hotmail.com>
2019-03-08 15:25:41 -0500

    Fix marcel model math

--- modules/models/marcel.py
+++ modules/models/marcel.py
@@ -3,6 +3,7 @@ import numpy as np
 import modules.data_processing.data_cleaning as dc
 
 def runMarcel(marcelDF, stats, roster, league, dateRange):
+
     dates = dc.convertDateStringToInt(dateRange)
     firstDate = dates[1]
     secondDate = dates[1] - 1
@@ -34,9 +35,13 @@ def runMarcel(marcelDF, stats, roster, league, dateRange):
     rosterNames = list(roster["Name"])
     statsPlus = ["IP"]
     statsPlus.extend(stats)
-    stats1DF = season1DF[statsPlus]
-    stats2DF = season2DF[statsPlus]
-    stats3DF = season3DF[statsPlus]
+    stats1DF = season1DF[statsPlus].replace([np.inf, -np.inf], np.nan).dropna()
+    stats2DF = season2DF[statsPlus].replace([np.inf, -np.inf], np.nan).dropna()
+    stats3DF = season3DF[statsPlus].replace([np.inf, -np.inf], np.nan).dropna()
+
+    print(stats1DF["SO9"].max())
+    print(stats1DF["ERA"].max())
+    print(stats1DF["WHIP"].max())
 
     predictDF = pd.DataFrame(index=rosterNames, columns=stats)
 
@@ -55,41 +60,45 @@ def marcelModel(stat, playerId, firstDf, secondDf, thirdDf, firstAvg, secondAvg,
         firstStat = firstData.loc[stat]
         firstIP = firstData.loc['IP']
     except KeyError:
-        firstStat = firstAvg[stat]
-        firstIP = firstAvg['IP']
+        firstStat = 0
+        firstIP = 0
     try:
         secondData = secondDf.loc[playerId]
         secondStat = secondData.loc[stat]
         secondIP = secondData.loc['IP']
     except KeyError:
-        secondStat = secondAvg[stat]
-        secondIP = secondAvg['IP']
+        secondStat = 0
+        secondIP = 0
     try:
         thirdData = thirdDf.loc[playerId]
         thirdStat = thirdData.loc[stat]
+        thirdIP = thirdData.loc['IP']
     except KeyError:
-        thirdStat = thirdAvg[stat]
+        thirdStat = 0
+        thirdIP = 0
 
     # calculate stat value used in calculations
-    statValue = (firstStat*3)+(secondStat*2)+(thirdStat*1)
+    projStatValue = (firstStat*3)+(secondStat*2)+(thirdStat*1)
 
     # starting pitcher innings pitched value used in calculations
-    IP = (firstIP * 0.5) + (secondIP * 0.1) + 60
+    projIP = (firstIP * 0.5) + (secondIP * 0.1) + 60
 
     # finding expected stat for each year incorporating total stat to total IP ratio
-    expFirst = firstAvg[stat] / firstAvg['IP'] * firstStat * 3
-    expSecond = secondAvg[stat] / secondAvg['IP'] * secondStat * 2
-    expThird = thirdAvg[stat] / thirdAvg['IP'] * thirdStat * 1
+    expFirst = firstDf[stat].sum() / firstDf['IP'].sum() * firstIP * 3
+    expSecond = secondDf[stat].sum() / secondDf['IP'].sum() * secondIP * 2
+    expThird = thirdDf[stat].sum() / thirdDf['IP'].sum() * thirdIP * 1
 
     # sum of the three seasons expected stat
-    expStat = expFirst + expSecond + expThird
+    expStatTotal = expFirst + expSecond + expThird
+    expIPTotal = (firstIP * 3) + (secondIP * 2) + (thirdIP * 1)
 
     # calculate stat per IP ratio
-    statPerIP = expStat / statValue
+    regressedStatTotal = expStatTotal / expIPTotal * 900
+
+    statPerIP = (regressedStatTotal + projStatValue) / (900 + expIPTotal) / 2
 
     # getting the predicted stat output using the stat per IP ratio and the predicted IP calculated
-    prediction = statPerIP * IP
-
+    prediction = statPerIP * projIP
     return prediction
 
 # If perGame equals 1, then the averages will be calculated per game
